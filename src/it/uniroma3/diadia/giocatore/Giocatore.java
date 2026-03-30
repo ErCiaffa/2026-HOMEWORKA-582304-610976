22987562db5b43b73a96b77c725295960cb34dc4
Ciaffa <ciaffaroni.alessio@gmail.com>
2026-03-30 21:48:38 +0200

    Aggiunte le nuovi classi richieste (n.b. non implementate).

---
+++ src/it/uniroma3/diadia/giocatore/Giocatore.java
@@ -0,0 +1,7 @@
+package it.uniroma3.diadia.giocatore;
+
+public class Giocatore {
+    // TODO: Giocatore ha la responsabilità di gestire i CFU del giocatore e du memorizzare gli attrezzi in un oggetto istanza della classe Borsa
+    // e aggiungere un riferimento ad un'istanza di Giocatore nella classe Partita (che ovviamente dovrà essere liberata dalla responsabilità spostate nella nuova classe)
+    // >>(vedi codice nel pdf)
+}
