f238caa709ea0e11850a8d21b7085a107905630f
Ciaffa <ciaffaroni.alessio@gmail.com>
2026-03-30 20:16:34 +0200

    Aggiunti JUnit Test per Partita e Stanza.

---
+++ src/unit-test/PartitaTest.java
@@ -0,0 +1,73 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PartitaTest {
+
+    private Partita partita;
+    private Stanza stanzaTest;
+
+    @BeforeEach
+    void setUp() {
+        this.partita = new Partita();
+        this.stanzaTest = new Stanza("Test");
+    }
+
+    /* TEST VINTA */
+
+    @Test
+    void test_win_start() {
+        assertFalse(partita.vinta(), "Stato iniziale: vinta() deve essere false.");
+    }
+
+    @Test
+    void test_win_true() {
+        partita.setStanzaCorrente(partita.getStanzaVincente());
+        assertTrue(partita.vinta(), "Stanza vincente: vinta() deve essere true.");
+    }
+
+    @Test
+    void test_win_false() {
+        partita.setStanzaCorrente(stanzaTest);
+        assertFalse(partita.vinta(), "Stanza ordinaria: vinta() deve essere false.");
+    }
+
+    /* TEST ISFINITA */
+
+    @Test
+    void test_isFinita_cfu() {
+        partita.setCfu(0);
+        assertTrue(partita.isFinita(), "CFU esauriti: isFinita() deve essere true.");
+    }
+
+    @Test
+    void test_isFinita_vittoria() {
+        partita.setStanzaCorrente(partita.getStanzaVincente());
+        assertTrue(partita.isFinita(), "Partita vinta: isFinita() deve essere true.");
+    }
+
+    @Test
+    void test_isFinita_manuale() {
+        partita.setFinita();
+        assertTrue(partita.isFinita(), "Flag fine impostato: isFinita() deve essere true.");
+    }
+
+    /* TEST STANZA CORRENTE */
+
+    @Test
+    void test_setStanzaCorrente() {
+        partita.setStanzaCorrente(stanzaTest);
+        assertSame(stanzaTest, partita.getStanzaCorrente(), "Errore nel riferimento stanzaCorrente.");
+    }
+
+    @Test
+    void test_setStanzaCorrente_null() {
+        partita.setStanzaCorrente(null);
+        assertNull(partita.getStanzaCorrente(), "Riferimento null non gestito correttamente.");
+    }
+
+    @Test
+    void test_getStanzaVincente() {
+        assertNotNull(partita.getStanzaVincente(), "Stanza vincente non inizializzata.");
+    }
+}
