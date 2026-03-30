3c3baeb6ce486d6439e735c5792968834eab720e
Ciaffa <ciaffaroni.alessio@gmail.com>
2026-03-30 21:48:38 +0200

    Aggiunte le nuovi classi richieste (n.b. non implementate).

---
+++ src/it/uniroma3/diadia/ambienti/Labirinto.java
@@ -0,0 +1,8 @@
+package it.uniroma3.diadia.ambienti;
+
+public class Labirinto {
+    // TODO: Introdurre la classe Labirinto
+    // 1- ha la responsabilità di creare il labirinto, di memorizzare la stanza iniziale (etrata) e quella finale (uscita).
+    // 2– aggiungere un riferimento ad un'istanza di Labirinto nella classe
+    // N.B. Partita (che ovviamente dovrà essere liberata dalla responsabilità spostate nella nuova classe)
+}
