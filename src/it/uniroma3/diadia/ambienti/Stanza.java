3b15e048d0a3e239f0347fbda4c2e480838f194b
Ciaffa <ciaffaroni.alessio@gmail.com>
2026-03-31 15:26:09 +0200

    Fix Import Attrezzo

--- src/it/uniroma3/diadia/ambienti/Stanza.java
+++ src/it/uniroma3/diadia/ambienti/Stanza.java
@@ -1,5 +1,7 @@
 package it.uniroma3.diadia.ambienti;
 
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
 /**
  * Classe it.uniroma3.diadia.ambienti.Stanza - una stanza in un gioco di ruolo.
  * Una stanza e' un luogo fisico nel gioco.
@@ -165,7 +167,7 @@ public class Stanza {
 
 	/**
 	 * Rimuove un attrezzo dalla stanza (ricerca in base al nome).
-	 * @param nomeAttrezzo
+	 * @param attrezzo
 	 * @return true se l'attrezzo e' stato rimosso, false altrimenti
 	 */
 	public boolean removeAttrezzo(Attrezzo attrezzo) {
