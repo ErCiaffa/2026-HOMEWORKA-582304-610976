afe3b2714a46660d8167cd6d4ca512ef9ac777e7
david <david@DESKTOP-N73B9NH>
2026-05-22 13:13:16 +0200

    aggiunto il test di stanza magica ma non funzionano, in più non riesco a risolvere l'errore in stanza magica protected

---
+++ src/unit-test/StanzaMagicaTest.java
@@ -0,0 +1,45 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import it.uniroma3.diadia.ambienti.StanzaMagica;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+class StanzaMagicaTest {
+	
+	private StanzaMagica stanza;
+	private Attrezzo a;
+	
+
+	@Before
+	public void setUp() {
+		stanza = new StanzaMagica("Magica",2);	
+		a = new Attrezzo("TroppiAttrezzi",2);
+		stanza.addAttrezzo(new Attrezzo("lanterna",2));
+	}
+
+	//aggiunta di un attrezzo esistente
+	@Test
+	public void testAddAttrezzoEsistente() {
+		assertTrue(stanza.addAttrezzo(new Attrezzo("osso",2)));
+	}
+
+	//aggiunta di un attrezzo con soglia superata
+	@Test
+	public void testAddAttrezzoSogliaSuperata() {
+		stanza.addAttrezzo(new Attrezzo("osso",2));
+		stanza.addAttrezzo(a);
+		assertFalse(stanza.hasAttrezzo("TroppiAttrezzi"));
+	}
+	
+	@Test
+	public void testModificaAttrezzo_AddAttrezzoSogliaSuperata() {
+		stanza.addAttrezzo(new Attrezzo("osso",2));
+		stanza.addAttrezzo(a);
+		assertNotNull(stanza.getAttrezzo("izzerttAipporT"));
+	}
+
+}
