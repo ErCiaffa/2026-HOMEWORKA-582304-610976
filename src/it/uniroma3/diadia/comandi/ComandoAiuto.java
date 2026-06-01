33f358b5a43cd9bb6853d07196451e01b80409d5
Ciaffa <ciaffaroni.alessio@gmail.com>
2026-06-01 11:12:27 +0200

    @ Es.12: ComandoAiuto elenca i comandi automaticamente da AbstractComando
    - AbstractComando registra i nomi dei comandi in un Set statico nel costruttore
    - ComandoAiuto stampa la lista automatica invece dell array hard-coded
    - rimosso array elencoComandi da DiaDia
    - aggiunto AbstractComandoTest
    
    @

--- src/it/uniroma3/diadia/comandi/ComandoAiuto.java
+++ src/it/uniroma3/diadia/comandi/ComandoAiuto.java
@@ -2,19 +2,16 @@ package it.uniroma3.diadia.comandi;
 
 import it.uniroma3.diadia.Partita;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class ComandoAiuto extends AbstractComando {
-    
-	static final private String[] elencoComandi = {"vai", "aiuto", "fine","prendi","posa","guarda"};
-	
+
     @Override
     public void esegui(Partita partita) {
-    	for(int i=0; i< elencoComandi.length; i++) 
-			partita.getIO().mostraMessaggio(elencoComandi[i]+" ");
-		partita.getIO().mostraMessaggio("");
-    }
-    @Override
-    public String getNome() {
-        return "aiuto";
+        Set<String> nomi = AbstractComando.getNomiComandiDisponibili();
+        String elenco = nomi.stream().collect(Collectors.joining(", "));
+        partita.getIO().mostraMessaggio("Comandi disponibili: " + elenco);
     }
 }
 
